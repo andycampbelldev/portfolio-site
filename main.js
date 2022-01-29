af3e2c9ec2537b74ed3ffa1b6cdee671275f6241
Andrew Campbell <AndyC@andrews-mbp.home>
2022-01-28 23:02:19 -0800

    styling improvements to home page

--- main.js
+++ main.js
@@ -13,3 +13,25 @@ const toggleNav = () => {
 document.querySelectorAll('.toggle-nav').forEach(button => {
     button.addEventListener('click', toggleNav);
 });
+
+// profile photo easter egg ;)
+const profilePhoto = document.querySelector('#profile-photo');
+
+const hulkMode = () => {
+    profilePhoto.style.filter = 'hue-rotate(-267deg) saturate(1.3)';
+}
+
+const today = new Date;
+if (today.getDate() === 31 && today.getMonth() === 9) {
+    setTimeout(
+        hulkMode, 3000
+    )
+}
+
+document.querySelector('.fa-react').addEventListener('dblclick', evt => {
+    const github = document.querySelector('.fa-github');
+    github.addEventListener('dblclick', hulkMode);
+    setTimeout(() => {
+        github.removeEventListener('dblclick', hulkMode);
+    }, 2000)
+})
